7147cb21a37dcc663b65f0f626c11795a7102c17
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2021-07-12 19:54:26 +0900

    youtube 代替画像OK

--- src/components/home/Reason.vue
+++ src/components/home/Reason.vue
@@ -125,7 +125,7 @@ export default {
     var id = url.match(/[\/?=]([a-zA-Z0-9_-]{11})[&\?]?/)[1];
     iframe.before('<img src="http://img.youtube.com/vi/'+id+'/mqdefault.jpg" />').remove();
     $(this).on('click', function() {
-      $(this).after('<div class="youtube"><iframe src="https://www.youtube.com/embed/'+id+'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>').remove();
+      $(this).after('<div class="reason-explanationGroup-movie_inner youtube"><iframe src="https://www.youtube.com/embed/'+id+'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>').remove();
     });
   });
 });
