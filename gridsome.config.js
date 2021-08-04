e6fca793595c2611fe8e938d07e76a07f252a580
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2021-08-04 18:58:20 +0900

    ハイフンを利用

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 module.exports = {
   // siteName: `一般社団法人 能力開発研修センター 能セン`,
-  siteName: `ハイフンはデフォルト`,
+  siteName: `学び方を学ぶ-`,
   // titleTemplate: `%s | 能セン-学び方を学ぶ-`,
   // titleTemplate: '',
   siteUrl: `https://www.nouryoku.com`,
