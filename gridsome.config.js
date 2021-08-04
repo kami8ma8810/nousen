fa5d130125402ecd5615851d64b63f42e7384ed4
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2021-08-05 08:25:04 +0900

    gridsome.config元に戻した

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,10 +2,10 @@ const webpack = require('webpack');
 module.exports = {
   // siteName: `一般社団法人 能力開発研修センター 能セン`,
   siteName: `学び方を学ぶ-`,
-  // titleTemplate: `%s | 能セン-学び方を学ぶ-`,
+  titleTemplate: `%s | 能セン-学び方を学ぶ-`,
   // titleTemplate: '',
   siteUrl: `https://www.nouryoku.com`,
-  // siteDescription: `
+  siteDescription: `
   // 能セン(能力開発研修センター)では、電気、建築、設備関連を中心とした講習会を各地で開催しております。経験豊富な専門の講師が資格合格のサポートを致します。`,
   metadata: {
     siteOgImage: `https://www.nouryoku.com/ogp.png`,
